e22cda81c33f7b1d8511a72fb2f6bed4cbfb281f
Canciu Ionut - Cristian <icanci01@ucy.ac.cy>
2022-07-16 16:01:00 +0300

    Countries Controller Scaffolding - Deleted WeatherForecast

--- HotelListingAPI/Data/Country.cs
+++ HotelListingAPI/Data/Country.cs
@@ -1,12 +1,10 @@
-﻿namespace HotelListingAPI.Data
-{
-    public class Country
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string ShortName { get; set; }
+﻿namespace HotelListingAPI.Data;
 
-        
-        public virtual IList<Hotel> Hotels { get; set; }
-    }
+public class Country
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string ShortName { get; set; }
+
+    public virtual IList<Hotel> Hotels { get; set; }
 }
