0c0ab22a857f504e0e3d0cba23bc8dd8d060f53d
Canciu Ionut - Cristian <icanci01@ucy.ac.cy>
2022-07-16 14:16:52 +0300

    Seed Database with OnModelCreating

--- HotelListingAPI/Data/HotelListingDbContext.cs
+++ HotelListingAPI/Data/HotelListingDbContext.cs
@@ -1,16 +1,68 @@
-﻿using System.Diagnostics.Metrics;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 
-namespace HotelListingAPI.Data
+namespace HotelListingAPI.Data;
+
+public class HotelListingDbContext : DbContext
 {
-    public class HotelListingDbContext : DbContext
+    public HotelListingDbContext(DbContextOptions options) : base(options)
     {
-        public HotelListingDbContext(DbContextOptions options) : base(options)
-        {
-            
-        }
+    }
 
-        public DbSet<Hotel> Hotels { get; set; }
-        public DbSet<Country> Countries { get; set; }
+    public DbSet<Hotel> Hotels { get; set; }
+    public DbSet<Country> Countries { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // Create initial Countries
+        modelBuilder.Entity<Country>().HasData(
+            new Country
+            {
+                Id = 1,
+                Name = "United States",
+                ShortName = "US"
+            },
+            new Country
+            {
+                Id = 2,
+                Name = "Canada",
+                ShortName = "CA"
+            },
+            new Country
+            {
+                Id = 3,
+                Name = "Bahamas",
+                ShortName = "BS"
+            }
+        );
+
+        // Create initial Hotels
+        modelBuilder.Entity<Hotel>().HasData(
+            new Hotel
+            {
+                Id = 1,
+                Name = "Sandals Resort and Spa",
+                Address = "Negril",
+                CountryId = 1,
+                Rating = 4.5
+            },
+            new Hotel
+            {
+                Id = 2,
+                Name = "Hotel Grand",
+                Address = "Grand Palldium",
+                CountryId = 2,
+                Rating = 4
+            },
+            new Hotel
+            {
+                Id = 3,
+                Name = "Comfort Suites",
+                Address = "George Town",
+                CountryId = 3,
+                Rating = 4.7
+            }
+        );
     }
 }
